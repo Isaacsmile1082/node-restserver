ea43734322c1eac65d0a3f5b7476bf5048e0e52b
Angel <angeliss1082@gmail.com>
2020-12-19 10:05:43 -0600

    RestServer - usuario correccion

--- server/config/config.js
+++ server/config/config.js
@@ -20,7 +20,7 @@ if( process.env.NODE_ENV === 'dev' ){
 
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://user_admin:ndD23wJHrSay05XN@cluster0.etyhn.mongodb.net/test';
+    urlDB = 'mongodb+srv://user_admin:ndD23wJHrSay05XN@cluster0.etyhn.mongodb.net/cafe';
 }
 
 process.env.urlDB = urlDB
